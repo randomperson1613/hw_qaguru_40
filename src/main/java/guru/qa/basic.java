4f0ed7ca15cfa8e5cf5ac459e496d7edacc88ed9
Damdam <sadpirozhok@gmail.com>
2026-04-07 23:06:02 +0300

    Выполнил задание по арифметических операций,логическим операция с init и double

---
+++ src/main/java/guru/qa/basic.java
@@ -0,0 +1,24 @@
+package guru.qa;
+
+public class basic {
+    public static void main(String[] args) {
+        int a = 17;
+        int b = 5;
+        double c = 3.33;
+        double d = Double.MAX_VALUE;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        System.out.println(a / c);
+        System.out.println(a * c);
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        System.out.println(d * d);
+    }
+
+}
